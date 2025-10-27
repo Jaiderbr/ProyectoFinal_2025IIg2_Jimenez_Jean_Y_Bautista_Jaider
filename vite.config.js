4b3365b0be4dac9f3416b93610796dacd2e8a4b7
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2025-10-26 19:08:41 -0500

    Add GitHub Pages deployment configuration
    Set homepage and base path for GitHub Pages in package.json and vite.config.js. Added deploy script using gh-pages to automate deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ProyectoFinal_2025IIg2_Jimenez_Jean_Y_Bautista_Jaider/",
 })
